564567fe2a316f69354c259bc8e692d899dd7209
FrozenBurrito <f3rm3ndouza@outlook.com>
2020-03-06 17:41:55 -0600

    Add global styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,19 +4,26 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div className="center panel">
-            <h1>A react project a day</h1>
-
-            <div className="card">
-                <Link to="/carousel"><h2>Image carousel</h2></Link>
-                <h3>06/Mar/2020</h3>
-                <p>Simple image gallery with captions</p>
+            <div className="header">
+                <h1>A react project a day</h1>
+                By <a href="https://github.com/Frozen-Burrito">Frozen Burrito</a>
             </div>
 
-            <div className="card">
-                <Link to="/map"><h2>Google Maps Embed</h2></Link>
-                <h3>07/Mar/2020</h3>
-                <p>A component that loads maps from Google</p>
-            </div>
+            <Link to="/carousel">
+                <div className="card">
+                    <h2>Image carousel</h2>
+                    <h3>06/Mar/2020</h3>
+                    <p>Simple image gallery with captions</p>
+                </div>
+            </Link>
+
+            <Link to="/map">
+                <div className="card">
+                    <h2>Google Maps Embed</h2>
+                    <h3>07/Mar/2020</h3>
+                    <p>A component that loads maps from Google</p>
+                </div>
+            </Link>
         </div>
     )
 } 
