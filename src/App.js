04cdce43a2f0f4cfacfe4cece2d90ec1e6a7e690
FrozenBurrito <f3rm3ndouza@outlook.com>
2020-03-06 05:50:50 -0600

    Completed excercise 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+let style = {
+    margin: 'auto',
+    width: '500px'
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={style} className="App">
+            <Carousel>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
+                    <p className="legend">Hong Kong</p>
+                </div>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore" />
+                    <p className="legend">Singapore</p>
+                </div>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan" />
+                    <p className="legend">Japan</p>
+                </div>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="NYC" />
+                    <p className="legend">NYC</p>
+                </div>
+            </Carousel>
+        </div>
+    );
 }
 
 export default App;
